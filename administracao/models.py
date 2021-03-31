e5ca46b9ae377fada89559650e4968a7170eb100
Lucas Santos <leos9877@gmail.com>
2021-03-31 18:32:49 -0300

    modelando o models do app administracao

--- administracao/models.py
+++ administracao/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from cadastro.models import *
 
-# Create your models here.
+class Receita(models.Model):
+    pessoa = models.ForeignKey(Pessoa, on_delete=models.CASCADE)
+    remedio = models.ForeignKey(Remedio, on_delete=models.CASCADE)
+    intervalo = float() #intervalo q a pessoa irá tomar o remedio
+    data_inicio = date() #o dia q a pessoa vai começar a tomar o remedio
+    dosagem = int #a dosagem do remedio
+
+    class Meta:
+        verbose_name = "Receita"
+        verbose_name_plural = "Receitas"
+        db_table = "receita"
+
+    def __str__(self):
+        return self.remedio
+    
+class Agendamento(models.Model):
+    nome
+    receita = models.ForeignKey(Receita, on_delete=models.CASCADE)
+    horario_inicio = datetime() #o dia e hora q a pessoa começou a tomar o remedio
+    horario_termino = datetime()#o dia e hora q a pessoa terminou de tomar o remedio
+    concluido = boolean #caso foi concluido
+    reajuste = ?
+       
+    class Meta:
+        verbose_name = "Receita"
+        verbose_name_plural = "Receitas"
+        db_table = "receita"
+
+    def __str__(self):
+        return self.remedio
+    
+    
