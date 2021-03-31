0df74edc6ed4155b8c3bff9dc084cb3cae637d34
Lucas Santos <leos9877@gmail.com>
2021-03-31 18:21:04 -0300

    iniciando a modelagem do models

--- cadastro/models.py
+++ cadastro/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
-# Create your models here.
+class Pessoa(models.Model):
+
+    nome = models.CharField(
+        verbose_name = "Nome Completo:",
+        max_length=194,
+    )
+
+    cpf = models.CharField(
+        verbose_name = "CPF:",
+        max_length=11,
+        unique=True,
+    )
+
+    horario_criacao = models.DateTimeField(
+        verbose_name = "Horário do Cadastro no Sistema:",
+        auto_now_add=True,
+    )
+      
+    class Meta:
+        verbose_name = "Pessoa"
+        verbose_name_plural = "Pessoas"
+        db_table = "pessoas"
+
+    def __str__(self):
+        return self.nome
+    
+class Remedio(models.Model):
+    
+    nome = models.CharField(
+        verbose_name = "Nome:",
+        max_length=194,
+    )
+
+    descricao = models.TextField(
+        verbose_name = "Descrição:",
+        max_length=340,
+    )
+    
+    class Meta:
+        verbose_name = "Remédio"
+        verbose_name_plural = "Remédios"
+        db_table = "remedio"
+
+    def __str__(self):
+        return self.nome
