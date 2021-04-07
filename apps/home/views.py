d9d71f89c47ecc949105362e49c22306a9a539a8
Lucas Santus <leos9877@gmail.com>
2021-04-07 14:34:23 -0300

    implementando index

--- apps/home/views.py
+++ apps/home/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render, redirect
-from .models import *
+from cadastro.models import Receita
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
 @login_required
 def index(request):
+
+    list_receitas = Receita.objects.all()
+
+    context = {
+        "nome_pagina": "Home",
+        "list_receitas": list_receitas,
+    }
+
     return render(request, "home/index.html")
