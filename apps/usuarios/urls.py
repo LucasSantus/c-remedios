61c4d8efa94c37be5182da5346c8ca096b373448
Lucas Santos <leos9877@gmail.com>
2021-12-01 10:44:43 -0300

    :pushpin: implementando tela de visualizaçao de perfis de usuario

--- apps/usuarios/urls.py
+++ apps/usuarios/urls.py
@@ -3,16 +3,19 @@ from django.contrib.auth import views as auth_views
 from .views import *
 
 urlpatterns = [
-    #SIGN UP
+    # SIGN UP
     path('usuario/signup/', SignUpView.as_view(), name='signup'),
     
-    #LOGIN
+    # LOGIN
     path("usuario/login/", auth_views.LoginView.as_view(template_name="usuarios/login/login.html"), name="login"),
     
-    #LOGOUT
+    # LOGOUT
     path("usuario/logout/", auth_views.LogoutView.as_view(template_name="usuarios/logout/logout.html"), name="logout"),
     
-    #RESET PASSWORD
+    # PROFILE
+    path('usuario/perfil/', perfil, name='perfil'),
+
+    # RESET PASSWORD
     path('usuario/password/reset/sent/', auth_views.PasswordResetDoneView.as_view(template_name="usuarios/reset_password/reset-password-done.html"), name="password_reset_done"),
     path('usuario/password/reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name="usuarios/reset_password/reset-password-confirm.html"), name="password_reset_confirm"),
     path('usuario/password/reset/complete/', auth_views.PasswordResetCompleteView.as_view(template_name="usuarios/reset_password/reset-password-complete.html"), name="password_reset_complete"),
--- apps/usuarios/views.py
+++ apps/usuarios/views.py
@@ -1,7 +1,17 @@
 from .models import *
 from .forms import *
 from django.views.generic.edit import CreateView
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
 
 class SignUpView(CreateView):
     template_name = 'usuarios/signup/signup.html'
     form_class = UsuarioForm
+
+@login_required
+def perfil(request):
+    form = UsuarioForm()
+    context = {
+        "form" : form,
+    }
+    return render(request, "usuarios/perfil/perfil.html", context)
