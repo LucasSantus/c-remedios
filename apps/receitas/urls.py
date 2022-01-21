7aabaf5deda88b3768e0ea6adaecf197dc456cf7
Lucas Santos <leos9877@gmail.com>
2022-01-21 08:22:57 -0300

    :pushpin: modificando codigos

--- apps/receitas/urls.py
+++ apps/receitas/urls.py
@@ -3,16 +3,21 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    # REGISTRAR
+    # RECEITA
     path("receita/registrar/<int:id_medico_paciente>", registrar_receita, name="registrar_receita"),
+
+    # REMÉDIO
     path("remedio/registrar/", registrar_remedio, name="registrar_remedio"),
-    path('dosagens-usuario/<int:id_receita>/', dosagem_usuario, name="dosagem_usuario"),
-    path('configurar-horarios-dosagens/<int:id_receita>', configura_horario_dosagem, name="configura_horario_dosagem"),
-    
+    path("remedios/listar/", listar_remedios, name="listar_remedios"),
+
+    # PACIENTE
     path('paciente/registrar/', registrar_paciente, name="registrar_paciente"),
 
     # LISTAR
     path("receita/listar/<int:id_medico_paciente>", listar_receitas, name="listar_receitas"),
+    
+    path('dosagens-usuario/<int:id_receita>/', dosagem_usuario, name="dosagem_usuario"),
+    path('configurar-horarios-dosagens/<int:id_receita>', configura_horario_dosagem, name="configura_horario_dosagem"),
     
     # EDITAR
     # path("receita/editar/<int:id_receita>/", editar_receita, name="editar_receita"),
@@ -21,7 +26,4 @@ urlpatterns = [
     # DELETAR
     # path("receita/deletar/<int:id_receita>/", deletar_receita, name="deletar_receita"),
     # path("remedio/deletar/<int:id_remedio>/", deletar_remedio, name="deletar_remedio"),
-    
-    # LISTAR
-    path("remedios/listar/", listar_remedios, name="listar_remedios"),
 ]
